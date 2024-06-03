2e4210efb5ad4897df5e60f891862f72e6eceeb9
sub-kek <subbkek@gmail.com>
2024-06-03 17:40:09 +0300

    Whitelist ticket front

--- src/config.h
+++ src/config.h
@@ -7,5 +7,5 @@ namespace FliwaBot::config {
     static const std::string &version_timestamp = "1717334664";
     static const std::string &guild_id = "1242839956074922004";
     static const std::string &author_id = "667782570955767829";
-    static const std::string &whitelist_tickets_category = "1242839956074922005";
+    static const std::string &whitelist_tickets_category = "1247155080269140018";
 }
--- src/form/whitelist_ticket_form.cpp
+++ src/form/whitelist_ticket_form.cpp
@@ -16,7 +16,7 @@ namespace FliwaBot {
                 .set_title("Заявка на сервер")
                 .add_component(
                         FliwaCord::component()
-                                .set_label("Ваш игровой ник")
+                                .set_label("Ваш игровой ник.")
                                 .set_id("nickname")
                                 .set_placeholder("Steve")
                                 .set_type(FliwaCord::cot_text)
@@ -27,7 +27,7 @@ namespace FliwaBot {
                 .add_row()
                 .add_component(
                         FliwaCord::component()
-                                .set_label("Ваш возвраст")
+                                .set_label("Ваш возвраст.")
                                 .set_id("age")
                                 .set_placeholder("15")
                                 .set_type(FliwaCord::cot_text)
@@ -35,16 +35,50 @@ namespace FliwaBot {
                                 .set_max_length(2)
                                 .set_min_length(1)
                                 .set_required(true))
+                .add_row()
+                .add_component(
+                        FliwaCord::component()
+                                .set_label("Откуда узнали о сервере?")
+                                .set_id("where_known")
+                                .set_placeholder("Пример: От друга <дискорд друга>")
+                                .set_type(FliwaCord::cot_text)
+                                .set_text_style(FliwaCord::text_paragraph)
+                                .set_max_length(128)
+                                .set_min_length(8)
+                                .set_required(true))
+                .add_row()
+                .add_component(
+                        FliwaCord::component()
+                                .set_label("Изучили ли вы правила сервера?")
+                                .set_id("studied_rules")
+                                .set_placeholder("Пример: Конечно")
+                                .set_type(FliwaCord::cot_text)
+                                .set_text_style(FliwaCord::text_short)
+                                .set_min_length(2)
+                                .set_max_length(16)
+                                .set_required(true))
+                .add_row()
+                .add_component(
+                        FliwaCord::component()
+                                .set_label("Если увидите нарушение со стороны игрока")
+                                .set_id("rules_breaking")
+                                .set_placeholder("Пример: Пожалуюсь администрации")
+                                .set_type(FliwaCord::cot_text)
+                                .set_text_style(FliwaCord::text_paragraph)
+                                .set_max_length(128)
+                                .set_min_length(8)
+                                .set_required(true))
                 .set_custom_id(get_id());
     }
 
     void whitelist_ticket_form::submit(const FliwaCord::form_submit_t &event) {
         std::string nickname = std::get<std::string>(event.components[0].components[0].value);
-        std::string age_str = std::regex_replace(std::get<std::string>(event.components[1].components[0].value),
-                                                 std::regex("[^0-9]+"), "");
+        std::string age_str = std::get<std::string>(event.components[1].components[0].value);
+        std::string where_known = std::get<std::string>(event.components[2].components[0].value);
+        std::string studied_rules = std::get<std::string>(event.components[3].components[0].value);
+        std::string rule_breaking = std::get<std::string>(event.components[4].components[0].value);
 
-        std::string channel_name = std::regex_replace(event.command.get_issuing_user().username,
-                                                      std::regex("#"), "\\#");
+        std::string channel_name = formatter::format("заявка-{0}", {event.command.get_issuing_user().username});
 
         bool can_create_ticket = true;
 
@@ -81,19 +115,33 @@ namespace FliwaBot {
         bot::core->channel_create(channel, [&](const FliwaCord::confirmation_callback_t &callback) {
             auto created = callback.get<FliwaCord::channel>();
 
+            bot::core->channel_edit_permissions(created.id, event.command.get_issuing_user().id,
+                                                FliwaCord::p_view_channel, 0x0, true);
+
             channel_mention = created.get_mention();
 
             FliwaCord::message ticket_message = FliwaCord::message()
                     .add_embed(
                             embed_builder::get_styled_embed()
                                     .set_title("Заявка в вайтлист")
-                                    .set_description(
-                                            formatter::format(
-                                                    "Никнейм: {0}\n"
-                                                    "Возвраст: {1}",
-                                                    {nickname, age_str}))
+                                    .add_field("Никнейм", nickname)
+                                    .add_field("Возвраст", age_str)
+                                    .add_field("Откуда узнали о сервере", where_known)
+                                    .add_field("Изучили ли вы правила сервера", studied_rules)
+                                    .add_field("Если увидите нарушение со стороны игрока?", rule_breaking)
                                     .add_field("Подающий", event.command.get_issuing_user().get_mention()))
-                    .set_channel_id(created.id);
+                    .set_channel_id(created.id)
+                    .add_component(
+                            FliwaCord::component()
+                                    .set_type(FliwaCord::cot_action_row)
+                                    .add_component(
+                                            FliwaCord::component()
+                                                    .set_type(FliwaCord::cot_button)
+                                                    .set_emoji("🔒")// :lock:
+                                                    .set_label("Закрыть")
+                                                    .set_id("ticket_close_button")
+                                                    .set_style(FliwaCord::cos_secondary))
+                    );
 
             bot::core->message_create(ticket_message);
 
@@ -102,7 +150,8 @@ namespace FliwaBot {
 
         locker->wait();
 
-        FliwaCord::message message = FliwaCord::message()
+        FliwaCord::message
+                message = FliwaCord::message()
                 .add_embed(
                         embed_builder::get_styled_embed()
                                 .set_title("Тикет создан")
